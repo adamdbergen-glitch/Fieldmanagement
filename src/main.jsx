cd0c4b55820fe6a78be4173caee7e85a104dc80c
adamdbergen-glitch <adamd.bergen@gmail.com>
2026-01-18 13:57:03 -0600

    Fix case sensitive import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import './index.css'
 
 // Unregister service workers to prevent stale cache issues
