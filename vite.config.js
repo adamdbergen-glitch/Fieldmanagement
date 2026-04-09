b4b6771eae6e20b52ec5321bd7b57cf5a976a7ec
adamdbergen-glitch <adamd.bergen@gmail.com>
2026-04-09 12:16:48 -0500

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // <--- THIS IS THE MAGIC LINE YOU ADDED
+  base: '/', // <--- THIS IS THE MAGIC LINE YOU ADDED
   plugins: [react({ jsxRuntime: 'automatic' })],
   
   server: {
