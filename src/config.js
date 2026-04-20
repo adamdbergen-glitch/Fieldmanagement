8b9c98db3dcfb26bb7300dde220f1cff2c508378
adamdbergen-glitch <adamd.bergen@gmail.com>
2026-04-20 11:34:44 -0500

    Change company phone number in config
    Updated company phone number in APP_CONFIG.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 export const APP_CONFIG = {
   appName: "The Paving Stone Pros",
   tagline: "Job Manager",
-  companyPhone: "204-555-0123", // Used in the Portal
+  companyPhone: "204-803-6464", // Used in the Portal
   
   // Theme Colors (We can use these to override standard colors later)
   primaryColor: "amber", 
